39a2f5cc77122bbc1e8a0a7fac568dfc5a371937
Matheus Campos <matheusproprentner@gmail.com>
2022-09-24 20:01:02 -0300

    feat: add table dashboard

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -1,13 +1,81 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../context/auth";
-import Header from "../../components/Header/"
+import Header from "../../components/Header/";
+import Title from "../../components/Title";
+import "./dashboard.css";
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from "react-icons/fi";
+import { Link } from "react-router-dom";
 function Dashboard(props) {
+  const [chamados, setChamados] = useState([1]);
   const { signed, loading, user, logout } = useContext(AuthContext);
   return (
     <div>
-      <Header></Header>
-      <h1>Pagina de dash {user.email}</h1>
-      <button onClick={logout}>sair</button>
+      <Header />
+
+      <div className="content">
+        <Title name="Atendimentos">
+          <FiMessageSquare size={25} />
+        </Title>
+
+        {chamados.length === 0 ? (
+          <div className="container dashboard">
+            <span>Nenhum chamado registrado...</span>
+
+            <Link to="/new" className="new">
+              <FiPlus size={25} color="#FFF" />
+              Novo chamado
+            </Link>
+          </div>
+        ) : (
+          <>
+            <Link to="/new" className="new">
+              <FiPlus size={25} color="#FFF" />
+              Novo chamado
+            </Link>
+
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col">Cliente</th>
+                  <th scope="col">Assunto</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Cadastrado em</th>
+                  <th scope="col">#</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td data-label="Cliente">Sujeito</td>
+                  <td data-label="Assunto">Suporte</td>
+                  <td data-label="Status">
+                    <span
+                      className="badge"
+                      style={{ backgroundColor: "#5cb85c" }}
+                    >
+                      Em aberto
+                    </span>
+                  </td>
+                  <td data-label="Cadastrado">20/06/2021</td>
+                  <td data-label="#">
+                    <button
+                      className="action"
+                      style={{ backgroundColor: "#3583f6" }}
+                    >
+                      <FiSearch color="#FFF" size={17} />
+                    </button>
+                    <button
+                      className="action"
+                      style={{ backgroundColor: "#F6a935" }}
+                    >
+                      <FiEdit2 color="#FFF" size={17} />
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </>
+        )}
+      </div>
     </div>
   );
 }
