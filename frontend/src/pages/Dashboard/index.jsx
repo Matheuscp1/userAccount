36e320c5a67b512ee1ef9fb3527d6b724e60dc34
Matheus Campos <matheusproprentner@gmail.com>
2022-09-24 23:05:12 -0300

    feat: add called in dashboard

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -1,13 +1,72 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/auth";
 import Header from "../../components/Header/";
 import Title from "../../components/Title";
 import "./dashboard.css";
 import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import api from "../../services/api";
+import { toast } from "react-toastify";
 function Dashboard(props) {
-  const [chamados, setChamados] = useState([1]);
-  const { signed, loading, user, logout } = useContext(AuthContext);
+  const { token } = useContext(AuthContext);
+  const [chamados, setChamados] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    loadChamados();
+
+    return () => {};
+  }, []);
+
+  async function loadChamados() {
+    try {
+      let response = await api.get("called", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      let lista = [];
+      response.data.map((e) => {
+        lista.push({
+          id: e.id,
+          name: e.clientId.name,
+          complement: e.complement,
+          createdDate: e.createdDate,
+          status: e.status,
+          subject: e.subject,
+        });
+      });
+      console.log(response.data);
+      if (lista.length === 0) {
+        toast.error("Você deve cadastrar um novo chamado");
+        setChamados([]);
+        setLoading(false);
+        return;
+      }
+      setChamados(lista);
+      setLoading(false);
+    } catch (error) {
+      toast.error("Algo deu errado");
+      setLoading(false);
+      setChamados([]);
+    }
+  }
+
+  if (loading) {
+    return (
+      <div>
+        <Header />
+
+        <div className="content">
+          <Title name="Atendimentos">
+            <FiMessageSquare size={25} />
+          </Title>
+
+          <div className="container dashboard">
+            <span>Buscando chamados...</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
@@ -44,33 +103,48 @@ function Dashboard(props) {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td data-label="Cliente">Sujeito</td>
-                  <td data-label="Assunto">Suporte</td>
-                  <td data-label="Status">
-                    <span
-                      className="badge"
-                      style={{ backgroundColor: "#5cb85c" }}
-                    >
-                      Em aberto
-                    </span>
-                  </td>
-                  <td data-label="Cadastrado">20/06/2021</td>
-                  <td data-label="#">
-                    <button
-                      className="action"
-                      style={{ backgroundColor: "#3583f6" }}
-                    >
-                      <FiSearch color="#FFF" size={17} />
-                    </button>
-                    <button
-                      className="action"
-                      style={{ backgroundColor: "#F6a935" }}
-                    >
-                      <FiEdit2 color="#FFF" size={17} />
-                    </button>
-                  </td>
-                </tr>
+                {chamados.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td data-label="Cliente">{item.name}</td>
+                      <td data-label="Assunto">{item.subject}</td>
+                      <td data-label="Status">
+                        <span
+                          className="badge"
+                          style={{
+                            backgroundColor:
+                              item.status === "Aberto" ? "#5cb85c" : "#999",
+                          }}
+                        >
+                          {item.status}
+                        </span>
+                      </td>
+                      <td data-label="Cadastrado">
+                        {new Date(item.createdDate).toLocaleDateString(
+                          "pt-br",
+                          {
+                            minute: "2-digit",
+                            hour: "2-digit",
+                          }
+                        )}
+                      </td>
+                      <td data-label="#">
+                        <button
+                          className="action"
+                          style={{ backgroundColor: "#3583f6" }}
+                        >
+                          <FiSearch color="#FFF" size={17} />
+                        </button>
+                        <button
+                          className="action"
+                          style={{ backgroundColor: "#F6a935" }}
+                        >
+                          <FiEdit2 color="#FFF" size={17} />
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </>
@@ -79,5 +153,4 @@ function Dashboard(props) {
     </div>
   );
 }
-
 export default Dashboard;
